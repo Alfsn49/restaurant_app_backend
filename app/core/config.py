ff0ef405a225d155d5d11958bc9fd15b1a5a1375
Alfsn49 <69661681+Alfsn49@users.noreply.github.com>
2026-01-09 16:44:31 -0500

    Actualizar la variable DATABASE_URL para utilizar la configuración correcta de Railway en lugar de Supabase.

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    DATABASE_URL: str = os.getenv("Database_supabase")
+    DATABASE_URL: str = os.getenv("Database_railway")
     SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecreto")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
